ed0e8cf6a5c319ddaab27c133be4ce09f1534f30
shehzeen <liaquatshehzeen@gmail.com>
2021-07-26 16:43:07 +0500

    added Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,94 +2,93 @@ import java.awt.event.*;
 import javax.swing.*; 
 import java.awt.*; 
 class calculator extends JFrame implements ActionListener { 
-    static JFrame f; 
-    static JTextField l; 
-    String s0, s1, s2;  
-    calculator() 
-    { 
-        s0 = s1 = s2 = ""; 
+static JFrame f; 
+static JTextField l; 
+String s0, s1, s2;  
+calculator() 
+{ 
+ s0 = s1 = s2 = ""; 
+} 
+public static void main(String args[]) 
+{ 
+f = new JFrame("calculator"); 
+  
+try { 
+UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
     } 
-    public static void main(String args[]) 
-    { 
-     f = new JFrame("calculator"); 
+catch (Exception e) { 
+System.err.println(e.getMessage()); 
+} 
+calculator c = new calculator();  
+l = new JTextField(16); 
+l.setEditable(false);  
+JButton s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, sa, ss, sd, sm, se, seq, seq1; 
   
-     try { 
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
-        } 
-        catch (Exception e) { 
-            System.err.println(e.getMessage()); 
-        } 
-  
-        calculator c = new calculator();  
-        l = new JTextField(16); 
-        l.setEditable(false);  
-        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1; 
-  
-        b0 = new JButton("0"); 
-        b1 = new JButton("1"); 
-        b2 = new JButton("2"); 
-        b3 = new JButton("3"); 
-        b4 = new JButton("4"); 
-        b5 = new JButton("5"); 
-        b6 = new JButton("6"); 
-        b7 = new JButton("7"); 
-        b8 = new JButton("8"); 
-        b9 = new JButton("9"); 
+        s0 = new JButton("0"); 
+        s1 = new JButton("1"); 
+        s2 = new JButton("2"); 
+        s3 = new JButton("3"); 
+        s4 = new JButton("4"); 
+        s5 = new JButton("5"); 
+        s6 = new JButton("6"); 
+        s7 = new JButton("7"); 
+        s8 = new JButton("8"); 
+        s9 = new JButton("9"); 
   
        
-        beq1 = new JButton("="); 
+        seq1 = new JButton("="); 
   
         
-        ba = new JButton("+"); 
-        bs = new JButton("-"); 
-        bd = new JButton("/"); 
-        bm = new JButton("*"); 
-        beq = new JButton("C"); 
+        sa = new JButton("+"); 
+        ss = new JButton("-"); 
+        sd = new JButton("/"); 
+        sm = new JButton("*"); 
+        seq = new JButton("C"); 
   
         
-        be = new JButton("."); 
+        se = new JButton("."); 
   
        
         JPanel p = new JPanel(); 
   
-        bm.addActionListener(c); 
-        bd.addActionListener(c); 
-        bs.addActionListener(c); 
-        ba.addActionListener(c); 
-        b9.addActionListener(c); 
-        b8.addActionListener(c); 
-        b7.addActionListener(c); 
-        b6.addActionListener(c); 
-        b5.addActionListener(c); 
-        b4.addActionListener(c); 
-        b3.addActionListener(c); 
-        b2.addActionListener(c); 
-        b1.addActionListener(c); 
-        b0.addActionListener(c); 
-        be.addActionListener(c); 
-        beq.addActionListener(c); 
-        beq1.addActionListener(c); 
+        sm.addActionListener(c); 
+        sd.addActionListener(c); 
+        ss.addActionListener(c); 
+        sa.addActionListener(c); 
+        s9.addActionListener(c); 
+        s8.addActionListener(c); 
+        s7.addActionListener(c); 
+        s6.addActionListener(c); 
+        s5.addActionListener(c); 
+        s4.addActionListener(c); 
+        s3.addActionListener(c); 
+        s2.addActionListener(c); 
+        s1.addActionListener(c); 
+        s0.addActionListener(c); 
+        se.addActionListener(c); 
+        seq.addActionListener(c); 
+        seq1.addActionListener(c); 
  
         p.add(l); 
-        p.add(ba); 
-        p.add(b1); 
-        p.add(b2); 
-        p.add(b3); 
-        p.add(bs); 
-        p.add(b4); 
-        p.add(b5); 
-        p.add(b6); 
-        p.add(bm); 
-        p.add(b7); 
-        p.add(b8); 
-        p.add(b9); 
-        p.add(bd); 
-        p.add(be); 
-        p.add(b0); 
-        p.add(beq); 
-        p.add(beq1);
+        p.add(sa); 
+        p.add(s1); 
+        p.add(s2); 
+        p.add(s3); 
+        p.add(ss); 
+        p.add(s4); 
+        p.add(s5); 
+        p.add(s6); 
+        p.add(sm); 
+        p.add(s7); 
+        p.add(s8); 
+        p.add(s9); 
+        p.add(sd); 
+        p.add(se); 
+        p.add(s0); 
+        p.add(seq); 
+        p.add(seq1);
   
-        p.setBackground(Color.blue);  
+        p.setBackground(Color.yellow);  
         f.add(p); 
   
         f.setSize(200, 220); 
